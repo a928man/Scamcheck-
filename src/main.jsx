e959d6ae50f35f2ddc79aa6ec8b165d8413bef7d
a928man <a928man@gmail.com>
2026-08-14 14:28:25 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -4,7 +4,7 @@ import App from "./App.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <div style={{ display: "flex", justifyContent: "center", padding: "24px 12px" }}>
+    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100dvh", padding: "12px 8px", boxSizing: "border-box" }}>
       <App />
     </div>
   </React.StrictMode>
