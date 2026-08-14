1a2e9261f49737b14cf6605d6a88ba941781cae1
a928man <a928man@gmail.com>
2026-08-14 12:11:53 +0300

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <div style={{ display: "flex", justifyContent: "center", padding: "24px 12px" }}>
+      <App />
+    </div>
+  </React.StrictMode>
+);
